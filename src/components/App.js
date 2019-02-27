ddc77c63aaa74a26957b96e7a21fef3a294000c1
Stanley Louis <stanlouis@msn.com>
2019-02-27 10:48:26 -0500

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,23 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
-        <Route path="/streams/delete" component={StreamDelete} />
-        <Route path="/streams/show" component={StreamShow} />
-      </React.Fragment>
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <React.Fragment>
+          <Header />
+
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </React.Fragment>
+      </BrowserRouter>
+    </div>
   );
 };
 
