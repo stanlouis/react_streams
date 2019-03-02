82a8d03edf504d896a285df5524d61c4ca414356
Stanley Louis <stanlouis@msn.com>
2019-03-02 04:43:02 -0500

    Add react portal and reusable modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
-  return <div>StreamCreate</div>;
+  const actions = (
+    <Fragment>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </Fragment>
+  );
+  return (
+    <Fragment>
+      StreamCreate
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </Fragment>
+  );
 };
 
 export default StreamDelete;
